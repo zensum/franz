c638750219ac939a522c58527f6a08e001fffdc2
William Rudenmalm <me@whn.se>
2017-07-19 16:32:23 +0200

    Add producer loop for testing

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,20 @@
 package franz
 
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerRecord
+
+fun createProducer(host: String) = mapOf(
+        "bootstrap.servers" to listOf(host),
+        "key.serializer" to "org.apache.kafka.common.serialization.StringSerializer",
+        "value.serializer" to "org.apache.kafka.common.serialization.StringSerializer",
+        "acks" to "all",
+        "compression.type" to "gzip",
+        "timeout.ms" to "10000",
+        "request.timeout.ms" to "10000",
+        "metadata.fetch.timeout.ms" to "5000",
+        "retries" to "0"
+).let { KafkaProducer<String, String>(it) }
+
 fun main(args: Array<String>) {
     WorkerBuilder()
             .subscribedTo("my-topic")
@@ -14,4 +29,10 @@ fun main(args: Array<String>) {
                 }
             }
             .start()
+
+    val p = createProducer("127.0.0.1:9092")
+    while(true) {
+        p.send(ProducerRecord("my-topic", "foo", "lol"))
+        Thread.sleep(500)
+    }
 }
