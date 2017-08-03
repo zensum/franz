12be8045714238b0256321f9c2e0e3b4a232e600
Anton Österberg <anton.osterberg@gmail.com>
2017-08-03 15:35:09 +0200

    Implemented class JobState that allows easier chaining/piping of instructions.

---
+++ src/main/kotlin/JobState.kt
@@ -0,0 +1,64 @@
+package franz.internal
+
+fun <T, U> JobDSL<T, U>.asPipe(): JobState<U> = JobState(this.value)
+
+class JobState<U>(val value: U){
+    var status: JobStatus = JobStatus.Incomplete
+
+        get() = field
+
+        set(value) {
+            if(status == JobStatus.Incomplete)
+                field = value
+        }
+
+    fun inProgress(): Boolean = status == JobStatus.Incomplete
+
+    /**
+     * Use when an operation must succeed or it is considered a permanent failure and should not trigger a retry,
+     * like checking the validity of phone number or mail address.
+     */
+    fun validate(predicate: (U) -> Boolean): JobState<U> = process(JobStatus.PermanentFailure, predicate)
+
+    /**
+     * Use when an operation may fail in such a why that a retry should be scheduled, like an error that is
+     * a result of a network connectivity issue or similar. In other words, there is nothing in the job itself
+     * that is erroneous, only the conditions for when it was executed.
+     */
+    fun execute(predicate: (U) -> Boolean): JobState<U> = process(JobStatus.TransientFailure, predicate)
+
+    /**
+     *  Use when either outcome is regarded as a successful result. Most common example of this is when a
+     *  job has already been processed and is already found written in the idempotence store or similar.
+     *  Everything is in its order but the current job should not trigger any further action and resolve
+     *  to [JobStatus.Success].
+     */
+    fun confirm(predicate: (U) -> Boolean): JobState<U> = process(JobStatus.Success, predicate)
+
+    /**
+     * Use when conducting the final operation on the job. If it successfully done (the predicate returns true)
+     * the [JobState] will automatically be set as [JobStatus.Success] and return the [JobStatus] rather than
+     * the [JobState] itself, prohibiting that any more work is done on this job through this pipe. The returned
+     * [JobStatus] can never be [JobStatus.Incomplete] when returning from this function.
+     * */
+
+    fun end(predicate: (U) -> Boolean): JobStatus {
+        val state = process(JobStatus.TransientFailure, predicate)
+        if(state.inProgress())
+            state.status = JobStatus.Success
+        return state.status
+    }
+
+    private inline fun process(newStatus: JobStatus, predicate: (U) -> Boolean): JobState<U> {
+        if(inProgress() && !predicate(value))
+            this.status = newStatus
+        return this
+    }
+
+    fun <R> map(transform: (U) -> R): JobState<R> {
+        if(!inProgress())
+            throw IllegalStateException("Try to perform a map operation on job that is longer in progress")
+        val newVal: R = transform(value)
+        return JobState(newVal)
+    }
+}
---
+++ src/test/kotlin/JobStateTest.kt
@@ -0,0 +1,135 @@
+package franz.internal
+
+import org.apache.kafka.clients.consumer.Consumer
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.junit.jupiter.api.Test
+import org.junit.rules.ExpectedException
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+
+class JobStateTest {
+
+    private fun <T> consumerRecordOfValue(value: T): ConsumerRecord<String, T> {
+        return ConsumerRecord("topic", 0, 0L, "key", value)
+    }
+
+    private fun <U> jobFrom(value: U): JobDSL<String, U> = JobDSL(consumerRecordOfValue(value))
+
+    @Test
+    fun testCreateJobState() {
+        val job = jobFrom("1")
+        assertEquals("1", job.asPipe().value)
+    }
+
+    @Test
+    fun testValidateTrue() {
+        val job = jobFrom("1")
+        val status = job.asPipe()
+                .validate { true }
+                .validate { true }
+
+        assertEquals(JobStatus.Incomplete, status.status)
+    }
+
+    @Test
+    fun testValidateFalse() {
+        val job = jobFrom("1")
+        val status = job.asPipe()
+                .validate { true }
+                .validate { false }
+                .validate { true }
+
+        assertEquals(JobStatus.PermanentFailure, status.status)
+    }
+
+    @Test
+    fun testExecuteTrue() {
+        val job = jobFrom("1")
+        val status = job.asPipe()
+                .execute { true }
+                .execute { true }
+
+        assertEquals(JobStatus.Incomplete, status.status)
+    }
+
+    @Test
+    fun testExecuteFalse() {
+        val job = jobFrom("1")
+        val status = job.asPipe()
+                .execute { true }
+                .execute { false }
+                .execute { true }
+
+        assertEquals(JobStatus.TransientFailure, status.status)
+    }
+
+    @Test
+    fun testConfirmTrue() {
+        val job = jobFrom("1")
+        val status = job.asPipe()
+                .confirm { true }
+                .confirm { true }
+
+        assertEquals(JobStatus.Incomplete, status.status)
+    }
+
+    @Test
+    fun testConfirmFalse() {
+        val job = jobFrom("1")
+        val status = job.asPipe()
+                .confirm { true }
+                .confirm { false }
+                .confirm { true }
+
+        assertEquals(JobStatus.Success, status.status)
+    }
+
+    @Test
+    fun testMapSuccessful() {
+        val job = jobFrom("1")
+        val status = job.asPipe()
+                .validate { true }
+                .map(Integer::parseInt)
+                .validate { it == 1 }
+
+        assertEquals(1, status.value)
+    }
+
+    @Test
+    fun testMapException() {
+        val job = jobFrom("1")
+
+        assertFailsWith(IllegalStateException::class) {
+            job.asPipe()
+                    .validate {true}
+                    .validate {false}
+                    .map(Integer::parseInt)
+        }
+    }
+
+    @Test
+    fun testEndSuccessful() {
+        val job = jobFrom("1")
+
+        val status = job.asPipe()
+                .validate { true}
+                .execute { true }
+                .confirm { true }
+                .end { true }
+
+        assertEquals(JobStatus.Success, status)
+    }
+
+    @Test
+    fun testEndFailure() {
+        val job = jobFrom("1")
+
+        val status = job.asPipe()
+                .validate { true}
+                .execute { true }
+                .confirm { true }
+                .end { false }
+
+        assertEquals(JobStatus.TransientFailure, status)
+    }
+}
