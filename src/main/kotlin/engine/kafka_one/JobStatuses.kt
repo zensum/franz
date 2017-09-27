677dcb80067f650ba6ecdd7573222049df6361b7
Anton Österberg <anton.osterberg@gmail.com>
2017-09-27 16:09:10 +0200

    Fixed typo.

--- src/main/kotlin/engine/kafka_one/JobStatuses.kt
+++ src/main/kotlin/engine/kafka_one/JobStatuses.kt
@@ -7,7 +7,7 @@ import org.apache.kafka.common.TopicPartition
 
 private fun <K, V> Map<K, V>.getOrFail(k: K) = get(k)!!
 
-private fun findCommitableOffsets(x: Map<JobId, JobStatus>) = x
+private fun findCommittableOffsets(x: Map<JobId, JobStatus>) = x
         .toList()
         .groupBy { it.first.first }
         .map { (_, values) ->
@@ -24,7 +24,7 @@ data class JobStatuses<T, U>(
         private val records: Map<JobId, ConsumerRecord<T, U>> = emptyMap()
 ) {
     fun update(updates: Map<JobId, JobStatus>) = copy(jobStatuses = jobStatuses + updates)
-    fun committableOffsets() = findCommitableOffsets(jobStatuses)
+    fun committableOffsets() = findCommittableOffsets(jobStatuses)
     fun removeCommitted(committed: Map<TopicPartition, OffsetAndMetadata>) = if (committed.isEmpty()) this else
         copy(
                 jobStatuses = jobStatuses.filterKeys { (topicPartition, offset) ->
