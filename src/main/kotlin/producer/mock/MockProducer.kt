3f7fa22419a76fbee0f1208eaf8407483ef5f625
Bona <bjorn.fyrvall@gmail.com>
2018-01-30 16:29:02 +0100

    Added settable callback lambda for all mocked send methods in the mocked producer, for testing purposes.

--- src/main/kotlin/producer/mock/MockProducer.kt
+++ src/main/kotlin/producer/mock/MockProducer.kt
@@ -3,24 +3,25 @@ package frans.producer.mock
 import franz.producer.ProduceResult
 import franz.producer.ProduceResultF
 import franz.producer.Producer
-import franz.producer.kafka_one.KProduceResult
-import franz.producer.kafka_one.makeProducer
-import franz.producer.kafka_one.sendAsync
-import org.apache.kafka.clients.producer.KafkaProducer
+
 import org.apache.kafka.clients.producer.ProducerRecord
 import java.util.concurrent.CompletableFuture
 
 class MockProducer<K, V>(
     val doSendResult: ProduceResult = MockProduceResult(),
     val sendAsyncResult: ProduceResult = MockProduceResult(),
-    val sendRawResult: ProduceResult = MockProduceResult()
+    val sendRawResult: ProduceResult = MockProduceResult(),
+    val onSend: (V) -> Unit = {},
+    val onSendAsync: (V) -> Unit = {},
+    val onSendRaw: (V) -> Unit = {}
+
 ): Producer<K, V> {
     private fun doSend(rec: ProducerRecord<K, V>) =
-        CompletableFuture.supplyAsync { doSendResult }
+        CompletableFuture.supplyAsync { doSendResult }.also { onSend.invoke(rec.value()) }
     override fun sendAsync(topic: String, key: K?, value: V): ProduceResultF =
-        CompletableFuture.supplyAsync { sendAsyncResult }
+        CompletableFuture.supplyAsync { sendAsyncResult }.also { onSendAsync.invoke(value) }
     override fun sendRaw(rec: ProducerRecord<K, V>) =
-        CompletableFuture.supplyAsync { sendRawResult }
+        CompletableFuture.supplyAsync { sendRawResult }.also { onSendRaw.invoke(rec.value()) }
     override fun close() = Unit
 
 }
