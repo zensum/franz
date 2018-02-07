48e778477acfecd3ad283a23b9e527c242b32a24
Bona <bjorn.fyrvall@gmail.com>
2018-02-07 12:50:19 +0100

    Changed when messages are run.

--- src/main/kotlin/engine/mock/MockConsumerActor.kt
+++ src/main/kotlin/engine/mock/MockConsumerActor.kt
@@ -8,19 +8,8 @@ class MockConsumerActor<T, U>(private val messages : List<Message<T, U>>) : Cons
     private var handlers = mutableListOf<(Message<T, U>) -> Unit>()
     var results = mutableMapOf<Message<T, U>, JobStatus>()
 
-    override fun start() {
-        println("Start")
-        messages.forEach { m ->
-            println("Found message")
-            handlers.forEach { h ->
-                println("Handled message")
-                h(m)
-            }
-        }
-    }
-
-    override fun stop() {
-    }
+    override fun start() = Unit
+    override fun stop() = Unit
 
     override fun setJobStatus(msg: Message<T, U>, status: JobStatus) {
        results[msg] = status
@@ -29,6 +18,14 @@ class MockConsumerActor<T, U>(private val messages : List<Message<T, U>>) : Cons
     override fun subscribe(fn: (Message<T, U>) -> Unit) {
         println("Subscribe")
         handlers.add(fn)
+
+        messages.forEach { m ->
+            println("Found message")
+            handlers.forEach { h ->
+                println("Handled message")
+                h(m)
+            }
+        }
     }
 
     fun createFactory() =
