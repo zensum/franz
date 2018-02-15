fe2180d546a168e9fb6a4d7575ee0dac643dfd3f
Bona <bjorn.fyrvall@gmail.com>
2018-02-15 17:59:50 +0100

    Removed extra threads from Active mock consumer.

--- src/main/kotlin/engine/mock/MockActiveConsumerActor.kt
+++ src/main/kotlin/engine/mock/MockActiveConsumerActor.kt
@@ -8,25 +8,9 @@ import java.util.*
 class MockActiveConsumerActor<T, U>(val messageQueue : Queue<Message<T, U>>) : ConsumerActor<T, U> {
     var results = mutableMapOf<Message<T, U>, JobStatus>()
     private var handlers = mutableListOf<(Message<T, U>) -> Unit>()
-    private val workerThread = Thread {
-        try {
-            while (true) {
-                val m = messageQueue.poll()
-                handlers.forEach { h ->
-                    h(m)
-                }
-            }
-        }catch (e: InterruptedException){
-            // When interrupted, quit this thread
-        }
-    }
-    override fun start() {
-        workerThread.start()
-    }
 
-    override fun stop() {
-        workerThread.interrupt()
-    }
+    override fun start() = Unit
+    override fun stop() = Unit
 
     override fun setJobStatus(msg: Message<T, U>, status: JobStatus) {
         results[msg] = status
@@ -34,6 +18,11 @@ class MockActiveConsumerActor<T, U>(val messageQueue : Queue<Message<T, U>>) : C
 
     override fun subscribe(fn: (Message<T, U>) -> Unit) {
         handlers.add(fn)
+
+        while(messageQueue.isNotEmpty()){
+            val message = messageQueue.poll()
+            handlers.forEach { it.invoke(message) }
+        }
     }
 
     companion object {
