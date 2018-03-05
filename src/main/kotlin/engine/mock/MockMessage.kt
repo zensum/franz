f84cedb88cd7bc5df41fa1442c7720473488ad81
Daniel <daniel.torbacka@gmail.com>
2018-03-05 17:40:58 +0100

    Implements all method in mock message

--- src/main/kotlin/engine/mock/MockMessage.kt
+++ src/main/kotlin/engine/mock/MockMessage.kt
@@ -1,28 +1,29 @@
 package franz.engine.mock
 
 import franz.Message
+import java.time.Instant
 
-class MockMessage<T>(private val topic: String, private val value: T): Message<String, T> {
-    override fun offset(): Long {
-        TODO("not implemented")
-    }
+class MockMessage<T>(
+    private val offset: Long = 1L,
+    private val topic: String,
+    private val value: T,
+    private val headers: Array<Pair<String, ByteArray>> = emptyArray(),
+    private val key: String = "key:$offset",
+    private val timestamp: Long = Instant.now().epochSecond
+): Message<String, T> {
+    override fun offset(): Long = offset
     override fun value(): T = value
     override fun topic() = topic
 
-    override fun headers(): Array<Pair<String, ByteArray>> {
-        throw NotImplementedError()
-    }
+    override fun headers(): Array<Pair<String, ByteArray>> = headers
 
-    override fun key(): String {
-        throw NotImplementedError()
-    }
+    override fun key(): String = key
 
-    override fun headers(key: String): Array<ByteArray> {
-        throw NotImplementedError()
-    }
+    override fun headers(key: String): Array<ByteArray> = headers()
+    .filter { it.first == key }
+    .map { it.second }
+    .toTypedArray()
 
-    override fun timestamp(): Long {
-        throw NotImplementedError()
-    }
+    override fun timestamp(): Long = timestamp
 }
 
