92e15831a537ddc53a8e3335bf987c5e6d8034b2
Bona <bjorn.fyrvall@gmail.com>
2018-05-28 12:15:23 +0200

    Readded inline keywords to jobstate. Added keywords crossinline to workerfunction predicates.

--- src/main/kotlin/JobState.kt
+++ src/main/kotlin/JobState.kt
@@ -22,16 +22,16 @@ class JobState<U: Any> @PublishedApi internal constructor(val value: U?, val int
      * Use when an operation must succeed or it is considered a permanent failure and should not trigger a retry,
      * like checking the validity of phone number or mail address.
      */
-    fun require(predicate: (U) -> Boolean): JobState<U> = process(JobStatus.PermanentFailure, predicate)
-    fun require(msg: String, predicate: (U) -> Boolean): JobState<U> = process(JobStatus.PermanentFailure, predicate, msg)
+    inline fun require(crossinline predicate: (U) -> Boolean): JobState<U> = process(JobStatus.PermanentFailure,  predicate)
+    inline fun require(msg: String, crossinline predicate: (U) -> Boolean): JobState<U> = process(JobStatus.PermanentFailure, predicate, msg)
 
     /**
      * Use when an operation may fail in such a why that a retry should be scheduled, like an error that is
      * a result of a network connectivity issue or similar. In other words, there is nothing in the job itself
      * that is erroneous, only the conditions for when it was executed.
      */
-    fun execute(predicate: (U) -> Boolean): JobState<U> = process(JobStatus.TransientFailure, predicate)
-    fun execute(msg: String, predicate: (U) -> Boolean): JobState<U> = process(JobStatus.TransientFailure, predicate, msg)
+    inline fun execute(crossinline predicate: (U) -> Boolean): JobState<U> = process(JobStatus.TransientFailure, predicate)
+    inline fun execute(msg: String, crossinline predicate: (U) -> Boolean): JobState<U> = process(JobStatus.TransientFailure, predicate, msg)
 
     /**
      *  Use when either outcome is regarded as a successful result. Most common example of this is when a
@@ -39,14 +39,14 @@ class JobState<U: Any> @PublishedApi internal constructor(val value: U?, val int
      *  Everything is in its order but the current job should not trigger any further action and resolve
      *  to [JobStatus.Success].
      */
-    fun advanceIf(predicate: (U) -> Boolean): JobState<U> = process(JobStatus.Success, predicate)
-    fun advanceIf(msg: String, predicate: (U) -> Boolean): JobState<U> = process(JobStatus.Success, predicate, msg)
+    inline fun advanceIf(crossinline predicate: (U) -> Boolean): JobState<U> = process(JobStatus.Success, predicate)
+    inline fun advanceIf(msg: String, crossinline predicate: (U) -> Boolean): JobState<U> = process(JobStatus.Success, predicate, msg)
 
     /**
      * Use for modelling a side-effect which doesn't have a return status. This
      * function is equivalent to calling require with a function that always returns true.
      */
-    fun sideEffect(fn: (U) -> Unit): JobState<U> = execute { fn(it!!); true }
+    inline fun sideEffect(crossinline fn: (U) -> Unit): JobState<U> = execute { fn(it!!); true }
 
     /**
      * If the [JobState] is non-terminal mark it as a success
@@ -78,7 +78,7 @@ class JobState<U: Any> @PublishedApi internal constructor(val value: U?, val int
         return status
     }
 
-    fun process(newStatus: JobStatus, predicate: (U) -> Boolean, msg: String? = null): JobState<U> {
+    inline fun process(newStatus: JobStatus, crossinline predicate: (U) -> Boolean, msg: String? = null): JobState<U> {
         val lastInterceptor = WorkerInterceptor {
             if (inProgress() && !predicate(value!!)) {
                 msg?.let { log.debug("Failed on: $it") }
